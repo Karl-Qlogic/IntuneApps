57fed1ea47fdd430254b7266be01e2c4b577bcc6
Karl@Qlogic <karl.almqvist@qlogic.se>
2022-04-20 15:31:54 +0200

    Testing stuff

--- Qinstaller1.0.ps1
+++ Qinstaller1.0.ps1
@@ -1,8 +1,49 @@
-$Path = "C:\Users\kalmq\OneDrive - QLOGIC AB\Dokument\EndPoint Manager\Git-IntuneApps\1Password\1Password.txt"
-$values = Get-Content $Path | Out-String | ConvertFrom-StringData
-echo $values.ShortName
-echo $values.Description
-echo $values.MinimumOSVersion
-echo $values.Category
-echo $values.InstallParameters
-echo $values.InstallerUrl
+
+$Qsource = "https://raw.githubusercontent.com/Karl-Qlogic/IntuneApps/main/1Password/1Password.txt"
+$QFileName = [System.IO.Path]::GetFileName($Qsource)
+$QFolderDest = "C:\QlogicScripts\Qinstaller\"
+$QFileDest = ($QFolderDest + $QFileName)
+
+#Create folder
+if (test-path -path $QFolderDest -PathType Container) {
+    
+}
+else {
+    New-Item -ItemType Directory -Path $QFolderDest -Force
+}
+
+#Download QRecipe
+(New-Object Net.WebClient).DownloadFile($Qsource, $QFileDest)
+
+#Get variables from QRecipe
+$values = Get-Content $QFileDest | Out-String | ConvertFrom-StringData
+
+$ShortName = $values.ShortName
+$LongName = $values.LongName
+$Description = $values.Description
+$Publisher = $values.Publisher
+$AppVersion = $values.AppVersion
+$MinimumOSVersion = $values.MinimumOSVersion
+$Category = $values.Category
+$LogoUrl = $values.LogoUrl
+$InstallParameters = $values.InstallParameters
+$Detection = $values.Detection
+$UninstallerLocation = $values.UninstallerLocation
+$UninstallParameters = $values.UninstallParameters
+$InstallerUrl = $values.InstallerUrl
+
+
+
+echo $ShortName
+echo $LongName
+echo $Description
+echo $Publisher
+echo $AppVersion
+echo $MinimumOSVersion
+echo $Category
+echo $LogoUrl
+echo $InstallParameters
+echo $Detection
+echo $UninstallerLocation
+echo $UninstallParameters
+echo $InstallerUrl
